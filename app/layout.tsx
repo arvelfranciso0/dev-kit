3bddb1978e56915e5d0e06bb5c2ab8dd2a5d0f78
Arvel Francisco <franciscoarvel123@gmail.com>
2025-12-28 10:26:45 +0800

    fix: fix the footer layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <main className="h-screen">{children}</main>
-        <Footer />
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
